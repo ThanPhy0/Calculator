b50b2cce8b5123f5c56f9f96de4cd668dfb640e7
ThanPhy0 <than.twp972@gmail.com>
2022-07-09 01:45:12 +0630

    first commit

---
+++ src/application/Calculator.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Calculator {
+	public double Calculate(double num1, double num2, String operator) {
+		switch (operator) {
+		case "+":
+			return num1 + num2;
+		case "-":
+			return num1 - num2;
+		case "*":
+			return num1 * num2;
+		case "/":
+			return num1 / num2;
+		default:
+			return 0;
+		}
+	}
+}
